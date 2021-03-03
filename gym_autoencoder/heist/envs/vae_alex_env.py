d1a9969c4009b7f00029c896379a81b43e539b76
Delo971 <DLoran@web.de>
2021-03-03 10:21:16 +0100

    Removed debugged statements

--- gym_autoencoder/heist/envs/vae_alex_env.py
+++ gym_autoencoder/heist/envs/vae_alex_env.py
@@ -1,7 +1,6 @@
 from gym_autoencoder.heist.envs.auto_basic_env import AutoencoderBasicEnv
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+
+
 class VaritionalAlexEnv(AutoencoderBasicEnv):
     def __init__(self):
         super(VaritionalAlexEnv, self).__init__(path_stub='model_stubs.VAE_Alex',
