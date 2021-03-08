e900340c3c01df7792490c3f7ac70e643953656e
Delo971 <DLoran@web.de>
2021-03-08 11:43:48 +0100

    Small typo change in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(name='gym_autoencoder',
       version='0.1.8',
-      install_requires=['gym','procgen','torch>=1.7.0','opencv-python',"torchinfo","PyYAML","matplotlib,torchvision"],
+      install_requires=['gym','procgen','torch>=1.7.0','opencv-python',"torchinfo","PyYAML","matplotlib","torchvision"],
       author="Dennis Loran",
       packages=setuptools.find_packages(where='.'),
       package_data={'autoencoder-procgen': ['models/*.pt']}
