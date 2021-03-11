2dd0afd491701eff49be00774a7e63b56ff33fb9
Delo971 <DLoran@web.de>
2021-03-11 10:01:15 +0100

    Changed Setup File

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 import setuptools
 from setuptools import setup
 
+import os
 
 
 
 setup(name='gym_autoencoder',
-      version='0.1.8',
+      version='0.1.10',
       install_requires=['gym','procgen','torch>=1.7.0','opencv-python',"torchinfo","PyYAML","matplotlib","torchvision"],
       author="Dennis Loran",
-      packages=setuptools.find_packages(where='.'),
-      package_data={'autoencoder-procgen': ['models/*.pt']}
+      packages=["gym_autoencoder","model_stubs","gym_autoencoder.heist.envs"],
+      package_data={'gym_autoencoder.heist.envs': ['../../../models/*.pt']},
       )
